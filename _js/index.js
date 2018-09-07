5b316f8ad7c03a91de3f8d9511193ae11cd14efb
leofavre <contato@leofavre.com>
2018-09-07 14:32:33 -0300

    Update code snippets

--- _js/index.js
+++ _js/index.js
@@ -4,18 +4,18 @@ import Codenimation from "./Codenimation";
 let targetNode = document.getElementsByClassName("type-canvas")[0];
 
 let snippets = [
-	".type--code::after {\n\tcontent: \"\\00a0\";\n}",
+	".type--code::after {\n  content: \"\\00a0\";\n}",
 	"<link type=\"text/css\" rel=\"stylesheet\" href=\"index.css\">",
 	"<!DOCTYPE html>",
 	"const getActiveClass = isActive => isActive ? \"active\" : \"inactive\";",
-	".banner__title {\n\tfont-size: 80px;\n\tz-index: 1;\n}",
-	"@keyframes blink {\n\tto {\n\t\tvisibility: hidden;\n\t}\n}",
+	".banner__title {\n  font-size: 80px;\n  z-index: 1;\n}",
+	"@keyframes blink {\n  to {\n    visibility: hidden;\n  }\n}",
 	"const getCurrentTimeStamp = () => +new Date();",
-	".menu {\n\tdisplay: flex;\n\tflex-flow: row wrap;\n}",
+	".menu {\n  display: flex;\n  flex-flow: row wrap;\n}",
 	"const path = (evt.composedPath && evt.composedPath()) || evt.path;",
-	"if (evt.type === \"click\") {\n\ttrigger(\"point\");\n}",
+	"if (evt.type === \"click\") {\n  trigger(\"point\");\n}",
 	"<meta charset=\"UTF-8\">",
-	"/(?:\w+|\[.*?\])\.indexOf\(.*?\) ?(?:> -1|< 0)/g"
+	"/(import|export|from)\s'.*?\/node_modules\/((?:@.*\/)?.*?)\/.*?'/g"
 ];
 
 let appearances = ["smaller", "normal", "normal", "bigger", "bigger"];
